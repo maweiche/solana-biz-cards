8046861a99ac50a7d636d1fb8d843a0d41e36d32
Matt Weichel <maweiche@gmail.com>
2023-10-12 14:02:59 +0200

    added blank export to tailwind.config to correct build error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
@@ -15,3 +16,5 @@ module.exports = {
   },
   plugins: [],
 };
+
+export {};
